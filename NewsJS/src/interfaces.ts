a4dfec45f2ee544e2a223fa95e5a945d2b41778a
ImmelstronDev <wishandra.afk@gmail.com>
2022-12-10 15:42:54 +0300

    feat: add interface for news and refactoring news.ts

--- NewsJS/src/interfaces.ts
+++ NewsJS/src/interfaces.ts
@@ -2,3 +2,14 @@ export interface source {
     id: string;
     name: string;
 }
+
+export interface news {
+    author: string;
+    content: string;
+    description: string;
+    publishedAt: string;
+    source: source;
+    title: string;
+    url: string;
+    urlToImage: string;
+}

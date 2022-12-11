3bb1139ccd37c5dc57ebbd8d81c5eac774a94e99
ImmelstronDev <wishandra.afk@gmail.com>
2022-12-11 15:35:20 +0300

    feat: refactoring appLoader.ts and controller.ts

--- NewsJS/src/components/controller/controller.ts
+++ NewsJS/src/components/controller/controller.ts
@@ -1,7 +1,9 @@
+import { sourceView } from '../../interfaces';
+import { callback } from '../../types';
 import AppLoader from './appLoader';
 
 class AppController extends AppLoader {
-    getSources(callback) {
+    getSources(callback: callback<sourceView>) {
         super.getResp(
             {
                 endpoint: 'sources',
@@ -10,13 +12,13 @@ class AppController extends AppLoader {
         );
     }
 
-    getNews(e, callback) {
-        let target = e.target;
-        const newsContainer = e.currentTarget;
+    getNews(e: Event, callback: callback<sourceView>) {
+        let target = <HTMLElement>e.target;
+        const newsContainer = <HTMLElement>e.currentTarget;
 
         while (target !== newsContainer) {
             if (target.classList.contains('source__item')) {
-                const sourceId = target.getAttribute('data-source-id');
+                const sourceId = target.getAttribute('data-source-id') as string;
                 if (newsContainer.getAttribute('data-source') !== sourceId) {
                     newsContainer.setAttribute('data-source', sourceId);
                     super.getResp(
@@ -31,7 +33,7 @@ class AppController extends AppLoader {
                 }
                 return;
             }
-            target = target.parentNode;
+            target = target.parentNode as HTMLElement;
         }
     }
 }
